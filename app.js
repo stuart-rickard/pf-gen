8e60e9990349a7295e83ebcf0a640cf5f55aab0f
stuart-rickard <stuart@bau-dev.com>
2021-12-26 15:29:28 -0800

    accept comand line input to node.js code

---
+++ app.js
@@ -0,0 +1,12 @@
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(console.log);
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+  });
+};
+
+
+
+printProfileData(profileDataArgs);
