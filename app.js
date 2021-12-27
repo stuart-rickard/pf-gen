aae82ed10eeb9981c74df3b80d69128233eb2c6a
stuart-rickard <stuart@bau-dev.com>
2021-12-27 14:49:25 -0800

    generate html using node.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(console.log);
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-  });
-};
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
